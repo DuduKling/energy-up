15bcedb54790d50577ff6040a67fe657a56d2990
Eduardo Kling <eduardokmesiano@gmail.com>
2019-02-25 19:26:52 -0300

    Adicionado video da logo da energy se formando na tela de splash

--- app/src/main/java/com/dudukling/enelz/splashActivity.java
+++ app/src/main/java/com/dudukling/enelz/splashActivity.java
@@ -1,22 +1,46 @@
 package com.dudukling.enelz;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 public class splashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        final ImageView imageRemoveFlicker = findViewById(R.id.imageViewSplashRemoveFlicker);
+        imageRemoveFlicker.setVisibility(View.VISIBLE);
+
+        VideoView mVideoView = findViewById(R.id.videoViewSplashLogo);
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.enel;
+        mVideoView.setVideoURI(Uri.parse(path));
+        mVideoView.start();
+
+        Handler handle2 = new Handler();
+        handle2.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                imageRemoveFlicker.setVisibility(View.GONE);
+            }
+        }, 500);
+
+
+
         Handler handle = new Handler();
         handle.postDelayed(new Runnable() {
             @Override
             public void run() {
                 showLauncher();
             }
-        }, 1500);
+        }, 1800);
     }
 
     private void showLauncher() {
